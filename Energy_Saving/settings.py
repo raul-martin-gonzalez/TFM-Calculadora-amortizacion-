c5ad8bf24b1f01de53702a06d957e3040c9b65f0
Raul Martin <raulmartin1295@gmail.com>
2024-04-03 13:15:52 +0200

    cambiar allowed host settings a *

--- Energy_Saving/settings.py
+++ Energy_Saving/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=33u@#=64+&!km8^lbv%$l&)98fym(p@+t^&1rw7h!&t_&aclz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://app-django-opunkgc4ya-ew.a.run.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
