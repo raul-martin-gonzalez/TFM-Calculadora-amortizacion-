3075c2c08f66ee2df0ec06cb553efea298698cc3
Raul Martin <raulmartin1295@gmail.com>
2024-05-09 18:11:24 +0200

    otra prueba

--- Energy_Saving/settings.py
+++ Energy_Saving/settings.py
@@ -28,14 +28,15 @@ SECRET_KEY = 'django-insecure-=33u@#=64+&!km8^lbv%$l&)98fym(p@+t^&1rw7h!&t_&aclz
 DEBUG = True
 
 #Host
-# CLOUDRUN_SERVICE_URL = 'https://app-django-opunkgc4ya-ew.a.run.app/'
-# if CLOUDRUN_SERVICE_URL:
-#     ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
-#     CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
-#     SECURE_SSL_REDIRECT = True
-#     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-# else:
 ALLOWED_HOSTS = ["*"]
+CLOUDRUN_SERVICE_URL = 'https://app-django-opunkgc4ya-ew.a.run.app/'
+if CLOUDRUN_SERVICE_URL:
+    ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
+    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+else:
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
